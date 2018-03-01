b683d6f087dab93c0e50ac97421f6011def4086f
cab79 <cb802@cam.ac.uk>
2018-03-01 18:42:04 +0000

    up

--- SCRIPT_eeglab_analysis.m
+++ SCRIPT_eeglab_analysis.m
@@ -8,9 +8,9 @@ fieldtrip_path = 'C:\EEGLABtraining2018\fieldtrip-20180212'; % path to fieldtrip
 batchfun_path = 'C:\EEGLABtraining2018\EEGLABworkshops2018';
 support_path = 'C:\EEGLABtraining2018\EEGLABworkshops2018\supporting_functions';
 
-addpath(fieldtrip_path); ft_defaults; % NEW - changed order to to conflict with eeglab BIOSIG toolbox
+addpath(fieldtrip_path); ft_defaults; 
 addpath(genpath(eeglab_path)); 
-rmpath(genpath(fullfile(eeglab_path,'plugins\Biosig3.3.0'))); 
+rmpath(genpath(fullfile(eeglab_path,'plugins\Biosig3.3.0'))); % NEW - due to conflict between Fieldtrip and eeglab BIOSIG toolbox
 addpath(genpath(batchfun_path));
 addpath(genpath(support_path));
 %% 2. FOLDER AND FILENAME DEFINITIONS
@@ -62,8 +62,8 @@ S.downsample = 125;
 S.chan.addloc = 'C:\Data\Matlab\eeglab13_6_5b\plugins\dipfit2.3\standard_BESA\standard-10-5-cap385.elp'; % add channel locations from this path; or leave as ''
 S.chan.interp = [];
 S.chan.reref = 1;
-S.filter.incl = [0 0];%[0.1 100]; % FILTER - INCLUSIVE
-S.filter.notch = {};%{[45 55],[95 105]};
+S.filter.notch = {[45 55],[95 105]};
+S.filter.incl = [1 45];%[0.1 100]; % FILTER - INCLUSIVE
 S.epoch.markers = {'S  1' 'S  2' 'S  3' 'S  4' 'S  5' 'S  6' 'S  7' 'S  8' 'S  9'};
 S.epoch.addmarker = 1; % set to 1 to add markers if the above are not present: will use the first marker value
 S.epoch.timewin = [-0.2 0.3]; % peri-marker time window
@@ -71,8 +71,8 @@ S.epoch.detrend = 0;
 S.epoch.rmbase = 0;
 S.epoch.separate = {[]}; % index of markers to separate into files
 S.epoch.separate_suffix = {}; % suffixes to new file names: one per cell of S.epoch.separate
-S.FTrej = {[0 5]}; % high freq to identify noise, low freq to identify eye movement
-S.ICA = 0;
+S.FTrej = {[]}; % high freq to identify noise, low freq to identify eye movement
+S.ICA = 1;
 S.combinefiles = 1;
 % RUN
 S=eeglab_preprocess(S)
@@ -82,7 +82,7 @@ save(fullfile(S.setpath,'S'),'S'); % saves 'S' - will be overwritten each time t
 % Only run this part if you have already manually selected ICA components for removal
 % path to ICA processed data
 load(fullfile(S.setpath,'S'))
-S.loadext = 'combined.set';
+S.loadext = 'combined_manrej_ICA.set';
 S.ICAremove = 1; % remove ICA components (0 if already removed from data, 1 if selected but not removed)
 S.detrend = 0;
 S.rmbase = 1;
@@ -94,4 +94,58 @@ S.separate = {}; % index of markers to separate into files
 S.separate_suffix = {}; % suffixes to new files names: one per cell of S.epoch.separate
 % RUN
 S=eeglab_preprocess_afterICA(S)
+save(fullfile(S.setpath,'S'),'S'); % saves 'S' - will be overwritten each time the script is run, so is just a temporary variable
+
+%% 4. FREQUENCY ANALYSIS
+load(fullfile(S.setpath,'S'))
+% data
+S.loadext = 'epoched_cleaned.set';% generic file suffix
+S.subjects = {'P1'}; % either a single subject, or leave blank to process all subjects in folder
+S.sessions = {};
+S.blocks = {'ECA'}; % blocks to load (each a separate file) - empty means all of them, or not defined
+S.conds = {'_1_','_1O_','_10_','_10O_'}; % conditions to load (each a separate file) - empty means all of them, or not defined
+% general settings
+S.basewin = [-0.2 0]; % baseline window
+S.rmbase = 1; % remove baseline prior to frequency/ERP
+S.markers = {'S  1','S  2','S  3','S  4','S  5','S  6','S  7','S  8', 'S  9'}; % marker types to analyse
+S.combinemarkers = 0; %include all events in a single condition.
+% analysis type
+S.analysistype = 'Freq'; %'Freq','TF','Coh','ERP'
+% freq analysis settings
+S.freqtype = 'both'; % induced or both
+S.baselinetype = 'relative'; % for TF analysis only: 'absolute', 'relative', 'relchange', 'normchange'
+S.freqsrange = [6:1:14]; % select frequency range and resolution (if freq analysis). 1D array produces all freqs enteredl 2D array uses ranges, e.g. [0 4; 4 8; 8 13; 13 30; 30 40];
+S.mov_avg_trials = 20; % N trials: average EEG power over trials using a moving average window
+S.mov_avg_trials_step = 20;
+S.bootrep = 500;% Bootstraps repetitions (for coherence analysis only)
+S.ncycles=3; % number of cycles for wavelet analysis (may crash if too big)
+% ERP settings
+S.CSD.apply=0; % Apply CSD (leave as 0 unless you know what you are doing!)
+S.CSD.montage = 'C:\Data\NTIP\CSDmontage_64.mat';
+S=erp_freq_analysis(S)
+save(fullfile(S.setpath,'S'),'S'); % saves 'S' - will be overwritten each time the script is run, so is just a temporary variable
+
+%% 4. PLOT FREQUENCY
+close all
+load(fullfile(S.setpath,'S'))
+S.freqselect = 10;
+S.blockselect = 1;
+S.eventselect = 1:9;%[1:9];
+S.exclchan = [29 30];
+%block_reorder = 1:8; % no reordering
+%participant_eventorder = [1 1]; % normal or reversed ordering. One value per subject.
+% data operation 1
+S.op(1).type = 'event';
+S.op(1).operation = 'normchange' %'subtract', 'relative', 'relchange', 'normchange' or 'db'
+S.op(1).grouping = [1 5; 2 6; 3 7; 4 8]; % e.g. for subtraction or dividing
+% data operation 2
+S.op(2).type = 'event';
+S.op(2).operation = 'subtract' %'subtract', 'relative', 'relchange', 'normchange' or 'db'
+S.op(2).grouping = [3 1; 4 2]; % e.g. for subtraction or dividing
+% data operation 3
+%S.op(3).type = 'freq';
+%S.op(3).operation = 'subtract'
+%S.op(3).grouping = S.freqsrange; % index of frequencies to use for comparison (will exclude the selected freq)
+% RUN
+S=erp_freq_analysis_plot(S)
 save(fullfile(S.setpath,'S'),'S'); % saves 'S' - will be overwritten each time the script is run, so is just a temporary variable
